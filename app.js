03e9a2d0fc6f0d6b3651d2d44cda6802d237bd40
Javier Lopez <jvrlpzd@gmail.com>
2022-12-12 11:28:13 +0100

    Updating for quest Express 7

--- app.js
+++ app.js
@@ -23,12 +23,16 @@ app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 const userHandlers = require("./userHandlers");
+const { hashPassword } = require("./auth.js");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
+// app.post("/api/users", userHandlers.postUser);
+app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+
+
 
 app.listen(port, (err) => {
   if (err) {
